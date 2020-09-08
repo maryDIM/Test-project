e456e2b91bad7cb0869c7155c38a0f09ff58a48a
ms.smirnova1 <ms.smirnova1@tensor.ru>
2020-09-08 17:46:41 +0300

    методы на отсутствие элемента

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,9 @@
 
 from selenium.webdriver import Remote as RemoteWebDriver
 from selenium.common.exceptions import NoSuchElementException
-
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class BasePage():
     def __init__(self, browser: RemoteWebDriver, url, timeout=10):
@@ -11,9 +13,29 @@ class BasePage():
 
     def open(self):
         self.browser.get(self.url)
+
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
         except NoSuchElementException:
             return False
+        return True
+
+    # абстрактный метод, который проверяет, что элемент не появляется на странице в течение заданного времени:
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+    # Если мы хотим проверить, что какой-то элемент исчезает, то следует воспользоваться явным ожиданием вместе с функцией until_not
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
         return True
