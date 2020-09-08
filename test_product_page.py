f0a8545095ddc4839b0defc73c232728fc513667
ms.smirnova1 <ms.smirnova1@tensor.ru>
2020-09-08 17:49:53 +0300

    Автопроверки на отсутствие эоемента

--- test_product_page.py
+++ test_product_page.py
@@ -21,5 +21,30 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code() # пройти тест и получить код в консоли
     page.should_item_in_basket()
 
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_item_to_basket()
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_item_to_basket()
+    page.should_success_message_disappeared()
+
+
+
 
 
