b074ffd95a641f80fb1f6b1773e8dbd24fc43e38
ms.smirnova1 <ms.smirnova1@tensor.ru>
2020-09-16 16:54:50 +0300

    добавлена проверка при переходе в корзину на ее пустоту

--- test_main_page.py
+++ test_main_page.py
@@ -1,22 +1,37 @@
+
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()  # открываем страницу
-    page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+#     page.open()  # открываем страницу
+#     page.go_to_login_page()  # выполняем метод страницы - переходим на страницу логина
+#     login_page = LoginPage(browser, browser.current_url)
+#     login_page.should_be_login_page()
+#
+# def test_guest_should_see_login_link(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.should_be_login_link()
+#
+# def test_guest_should_see_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+#     page = LoginPage(browser, link)
+#     page.open()
+#     page.should_be_login_page()
 
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    # Гость открывает страницу товара
+    link = "http://selenium1py.pythonanywhere.com/en-gb/"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
-
-def test_guest_should_see_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
+    # Переходит в корзину по кнопке в шапке сайта
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    # Ожидаем, что в корзине нет товаров
+    basket_page.should_no_items_in_basket()
+    # Ожидаем, что есть текст о том что корзина пуста
+    basket_page.should_success_message_about_empty_basket()
