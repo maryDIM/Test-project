e21dea0d0defeb51532e403c833a85a3c1aed8e1
ms.smirnova1 <ms.smirnova1@tensor.ru>
2020-09-16 16:56:57 +0300

    Добавлена проверка при переходе в корзину на ее пустоту

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 from pages.product_page import ProductPage
+from pages.basket_page import BasketPage
 
 # @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
 #                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -55,6 +56,25 @@ def test_guest_should_see_login_page_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    # Гость открывает страницу товара
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    # Переходит в корзину по кнопке в шапке сайта
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    # Ожидаем, что в корзине нет товаров
+    basket_page.should_no_items_in_basket()
+    # Ожидаем, что есть текст о том что корзина пуста
+    basket_page.should_success_message_about_empty_basket()
+
+
+
+
+
 
 
 
