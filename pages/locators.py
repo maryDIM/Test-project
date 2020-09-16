cfeb9dbd031d9a3bb2b72f731fa8322f0413429f
ms.smirnova1 <ms.smirnova1@tensor.ru>
2020-09-16 16:45:26 +0300

    Добавлены ссылки на пустую корзину

--- pages/locators.py
+++ pages/locators.py
@@ -2,11 +2,13 @@ from selenium.webdriver.common.by import By
 
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    # LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini .btn-default")
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+# class MainPageLocators():
 
 class ProductPageLocators(object):
     BASKET_TOTAL = (By.CSS_SELECTOR, ".alertinner p strong")
@@ -15,4 +17,13 @@ class ProductPageLocators(object):
     PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
     BASKET_ADD_BTN = (By.CLASS_NAME, "btn-add-to-basket")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alertinner strong")
+
+class BasketPageLocators():
+    BASKET_ITEMS = (By.CSS_SELECTOR, ".basket-items")
+    ITEM_BASKET = (By.CSS_SELECTOR, "#content_inner p")
+
+
+
+
+
 
